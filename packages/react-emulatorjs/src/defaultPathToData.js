c3eb902b5cb1aad041a826696223957ba009eada
dimitrikarpov <karpov.dimon@gmail.com>
2024-01-03 11:29:12 +0500

    remove obsolete js files

--- packages/react-emulatorjs/src/defaultPathToData.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultPathToData = void 0;
-exports.defaultPathToData = "https://cdn.jsdelivr.net/gh/EmulatorJS/EmulatorJS@latest/data";
