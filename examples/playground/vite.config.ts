54aaa6bae1d00452b6f67eee4254bb245ded36d8
dimitrikarpov <karpov.dimon@gmail.com>
2024-01-04 15:09:27 +0500

    11

--- examples/playground/vite.config.ts
+++ examples/playground/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/playground",
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
