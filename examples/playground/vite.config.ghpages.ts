ae4d534e9bcc5a5a900e26e6894377b7803c9f10
dimitrikarpov <karpov.dimon@gmail.com>
2024-01-04 14:15:04 +0500

    new alias

--- examples/playground/vite.config.ghpages.ts
+++ examples/playground/vite.config.ghpages.ts
@@ -10,9 +10,25 @@ export default defineConfig({
   plugins: [react()],
 
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "react-emulatorjs": require.resolve("react-emulatorjs"),
-    },
+    // alias: {
+    //   "@": path.resolve(__dirname, "./src"),
+    //   "react-emulatorjs": require.resolve("react-emulatorjs"),
+    // },
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+      {
+        find: "react-emulatorjs",
+        replacement: require.resolve("react-emulatorjs"),
+      },
+    ],
   },
 })
+// alias: [
+//   {
+//     find: /@mui\/material/,
+//     replacement: path.resolve(__dirname, 'node_modules', '@mui', 'material'),
+//   },
+// ],
