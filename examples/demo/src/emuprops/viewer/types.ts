1cf71b3dc9105ab9346290a93a4da279f18a89b3
dimitrikarpov <karpov.dimon@gmail.com>
2024-01-04 17:31:40 +0500

    remove demo app folder

--- examples/demo/src/emuprops/viewer/types.ts
+++
@@ -1,9 +0,0 @@
-import { Settings } from "react-emulatorjs"
-
-export type PropItemName = keyof Settings
-export type PropItemValue = Settings[PropItemName]
-
-export type PropItemProps = {
-  name: PropItemName
-  value: PropItemValue
-}
