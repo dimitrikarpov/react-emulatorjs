3d590988d0c7a742b31eec992eed0e6ad7ce52bb
dimitrikarpov <karpov.dimon@gmail.com>
2024-01-28 11:48:03 +0500

    update path to data urls

--- examples/playground/src/emuprops/builder/PathToDataInput.tsx
+++ examples/playground/src/emuprops/builder/PathToDataInput.tsx
@@ -12,7 +12,13 @@ export const PathToDataInput: React.FunctionComponent<Props> = ({
 
   return (
     <div className="flex flex-col flex-wrap gap-3">
-      {[defaultPathToData, "/data"].map((path) => (
+      {[
+        defaultPathToData,
+        "https://cdn.emulatorjs.org/stable/data",
+        "https://cdn.emulatorjs.org/latest/data",
+        "https://cdn.emulatorjs.org/nightly/data",
+        "/data",
+      ].map((path) => (
         <div key={path}>
           <input
             type="radio"
